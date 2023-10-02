a52767df02f7d60ab8e9b2a7e2943770cd7bf4ed
MonkeDbanana <137050109+MonkeDbanana@users.noreply.github.com>
2023-10-02 18:28:15 +1100

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,11 @@
+var first_name="ranbir";
+var last_name="kapoor";
+var first_name_name=last_name_name.concat(last_name);
+console.log (first_name)
+var a = 10;
+console.log (a)
+var b = 5;
+console.log (b)
+
+var add= a + b;
+console.log(add);
